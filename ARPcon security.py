4d48ec1c8460648826fc8a91b4c1fa54299b28a3
unknown <prrprrbro23@gmail.com>
2020-10-26 22:01:11 +0530

    second commit

--- ARPcon security.py
+++ ARPcon security.py
@@ -1,25 +1,54 @@
 import discord
 import asyncio
+from discord.ext.commands import Bot
+ 
+token = "NzYyMTkzNzkzMjU2MjU5NTg2.X3lmSQ.N4hE2oY4N-OMVAFopNbh5SmYcEs"
+client = Bot(command_prefix = '.')
 
-token = "NzYyMTkzNzkzMjU2MjU5NTg2.X3lmSQ.uDHYR-cmckeDH9ITa637yT5WXeQ"
-client = discord.Client()
 @client.event
 async def on_ready():
-	print(f'{client.user} has connected to Discord!')
+	print(f'Bot connected as {client.user}')
+	await client.change_presence(activity = discord.Game('Pubg'))
 
-@client.event
-async def on_member_join(member):
-	await member.create_dm()
-	await member.dm_channel.send(
-		f'Hi {member.name}, welcome to my Discord server!'
-	)
+@client.command(name='server', help='Gives Server Information')
+
+async def fetchServerInfo(ctx):
+	await ctx.send(f'Server Name: {ctx.guild.name}')
+	await ctx.send(f'Server Size: {ctx.guild}')
+	await ctx.send(f'Active People:{len(ctx.guild.member_count)}')
+
+# async def on_message(message):
+# 	if message.content == 'test':
+# 		await message.channel.send('Testing 1 2 3!')
+
+
+
+
+@client.command(name='ping', help='Tells you about your ping')
+async def ping(ctx):
+    await ctx.send(f'Pong! {round (client.latency * 1000)}ms ')
+
+
+
+
+#client = discord.Client()
+# @client.event
+# async def on_ready():print(ctx.guild.member_count)print(ctx.guild.member_count)
+# 	print(f'{client.user} has connected to Discord!')
+
+# @client.event
+# async def on_member_join(member):
+# 	await member.create_dm()
+# 	await member.dm_channel.send(
+# 		f'Hi {member.name}, welcome to my Discord server!'
+# 	)
     
-@client.event
-async def on_message(message):
-	if message.author == client.user:
-		return
-
-	if message.content.startswith('$hello'):
-	    await message.channel.send('Hello!')
+# @client.event
+# async def on_message(message):
+# 	if message.author == client.user:
+# 		return
+ 
+# 	if message.content.startswith('$hello'):
+# 	    await message.send('Hello!')
 
 client.run(token)
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
-.# this is a Discord Bot
+# this is a Discord Bot which is made in python
+## it is made for Arpcon ctf community 
+### so lets see if i could finish the project and gift them this bot
+# How to install the bot:
+``` git clone ** the repo name** ```
+``` isntall python ```
+``` pip install discord.py``` 
